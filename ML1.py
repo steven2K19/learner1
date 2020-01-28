b6eee1dbc95990228e1e6a48602601baaa15e2d3
Steven Wang <e_wanghe@hotmail.com>
2020-01-27 22:40:29 -0500

    Add files via upload

---
+++ ML1.py
@@ -0,0 +1,66 @@
+from pandas import read_csv
+import matplotlib as mpl
+mpl.use('Agg')
+from matplotlib import pyplot as plt
+from pandas.plotting import scatter_matrix
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import StratifiedKFold
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+from sklearn.ensemble import BaggingClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.ensemble import GradientBoostingClassifier
+
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+
+url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv"
+names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
+dataset = read_csv(url, names=names)
+# dataset.groupby('class').size()
+# dataset.plot(kind='box', subplots=True, layout=(2,2), sharex=False, sharey=False)
+# dataset.hist()
+# scatter_matrix(dataset)       # plt.savefig('1.png')
+array = dataset.values
+X = array[:,0:4]
+y = array[:,4]
+X_train, X_validation, Y_train, Y_validation = train_test_split(X, y, test_size=0.20, random_state=1)
+
+
+# Compare Algorithms
+models = []
+models.append(('LR', LogisticRegression(solver='liblinear', multi_class='ovr')))
+models.append(('NB', GaussianNB()))
+models.append(('LDA', LinearDiscriminantAnalysis()))
+models.append(('KNN', KNeighborsClassifier()))
+models.append(('SVM', SVC(gamma='auto')))
+models.append(('CART', DecisionTreeClassifier()))
+models.append(('BAG', BaggingClassifier(base_estimator=DecisionTreeClassifier(random_state=1), n_estimators=100, random_state=1)))
+models.append(('FOREST',RandomForestClassifier(n_jobs=2, random_state=0)))
+models.append(('ADA',AdaBoostClassifier(base_estimator=DecisionTreeClassifier(max_depth=2, random_state=1), n_estimators=20, random_state=1)))
+models.append(('GBoost',GradientBoostingClassifier()))
+results = []
+names = []
+for name, model in models:
+    kfold = StratifiedKFold(n_splits=10, random_state=1, shuffle=True)
+    cv_results = cross_val_score(model, X_train, Y_train, cv=kfold, scoring='accuracy')
+    results.append(cv_results)
+    names.append(name)
+    print('%s: %f (%f)' % (name, cv_results.mean(), cv_results.std()))
+# plt.boxplot(results, labels=names)    visualize result
+
+# prediction
+model = SVC(gamma='auto')
+model.fit(X_train, Y_train)
+predictions = model.predict(X_validation)
+print(accuracy_score(Y_validation, predictions))
+print(confusion_matrix(Y_validation, predictions))
+print(classification_report(Y_validation, predictions))
